56730d25cadafb9bddfb3839291feabc7fcc3ea7
beatriz-camilo <abeatrizcamilo@gmail.com>
2025-12-01 15:17:45 -0300

    Objetos de professores corrigido

--- projetoweb2/src/main/java/com/bti/projetoweb2/config/DatabaseSeeder.java
+++ projetoweb2/src/main/java/com/bti/projetoweb2/config/DatabaseSeeder.java
@@ -47,24 +47,28 @@ public class DatabaseSeeder implements CommandLineRunner {
             funcionarioRepository.save(f2);
         }
 
-        // if (professorRepository.count() == 0) {
+        if (professorRepository.count() == 0) {
 
-        //     Professor p1 = new Professor(
-        //             "Mariana Filho",
-        //             "99988877766",
-        //             "Biologia Marinha",
-        //             NivelAcademico.GRADUACAO
-        //     );
+            Professor p1 = new Professor(
+                "Mariana Filho",
+                "99988877766",
+                "Pesquisador",
+                TipoVinculo.TEMPORARIO,
+                "Biologia Molecular",
+                NivelAcademico.MESTRADO
+            );
 
-        //     Professor p2 = new Professor(
-        //             "Carlos Mendes",
-        //             "33322211100",
-        //             "Epedemiologia",
-        //             NivelAcademico.DOUTORADO
-        //     );
+            Professor p2 = new Professor(
+                "Carlos Mendes",
+                "33322211100",
+                "Pesquisador",
+                TipoVinculo.FIXO,
+                "Astrofísica",
+                NivelAcademico.DOUTORADO
+             );
 
-        //     professorRepository.save(p1);
-        //     professorRepository.save(p2);
-        // }
+             professorRepository.save(p1);
+             professorRepository.save(p2);
+         }
     }
 }

2fe2ee66ae4e7d8a1b1a03a10f59fcd3a003ef79
beatriz-camilo <abeatrizcamilo@gmail.com>
2025-12-02 21:28:52 -0300

    criação de pasta destinada às entidades de users, roles e DTOs

---
+++ projetoweb2/src/main/java/com/bti/projetoweb2/users/AutenticationDTO.java
@@ -0,0 +1,5 @@
+package com.bti.projetoweb2.users;
+
+public record AutenticationDTO(String login, String password) {
+    
+}
---
+++ projetoweb2/src/main/java/com/bti/projetoweb2/users/RegisterDTO.java
@@ -0,0 +1,5 @@
+package com.bti.projetoweb2.users;
+
+public record RegisterDTO(String login, String password, UserRole role) {
+    
+}

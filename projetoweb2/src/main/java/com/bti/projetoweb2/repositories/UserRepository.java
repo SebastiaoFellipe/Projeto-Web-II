4115c11497a761b2a94db681ccd84d8d8e9608fb
beatriz-camilo <abeatrizcamilo@gmail.com>
2025-12-02 23:29:05 -0300

    fix: correção no tipo de retorno

--- projetoweb2/src/main/java/com/bti/projetoweb2/repositories/UserRepository.java
+++ projetoweb2/src/main/java/com/bti/projetoweb2/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package com.bti.projetoweb2.repositories;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import com.bti.projetoweb2.users.User;
 
 
 public interface UserRepository extends JpaRepository<User, String> {
     
-    UserDetails findByLogin(String login);
+    Optional<User> findByLogin(String login);
 }

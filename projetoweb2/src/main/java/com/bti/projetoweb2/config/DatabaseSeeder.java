5cc7d6d12fd9020c182ea16166c702d087c01269
beatriz-camilo <abeatrizcamilo@gmail.com>
2025-12-02 23:24:58 -0300

    correção/criação de usuário testes para rota de login

--- projetoweb2/src/main/java/com/bti/projetoweb2/config/DatabaseSeeder.java
+++ projetoweb2/src/main/java/com/bti/projetoweb2/config/DatabaseSeeder.java
@@ -1,14 +1,16 @@
 package com.bti.projetoweb2.config;
 
+import com.bti.projetoweb2.users.User;
 import com.bti.projetoweb2.entities.Funcionario;
-import com.bti.projetoweb2.entities.TipoVinculo;
-import com.bti.projetoweb2.repositories.FuncionarioRepository;
 import com.bti.projetoweb2.entities.NivelAcademico;
 import com.bti.projetoweb2.entities.Professor;
+import com.bti.projetoweb2.entities.TipoVinculo;
+import com.bti.projetoweb2.repositories.FuncionarioRepository;
 import com.bti.projetoweb2.repositories.ProfessorRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bti.projetoweb2.repositories.UserRepository; 
+import com.bti.projetoweb2.users.UserRole;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,11 +18,19 @@ public class DatabaseSeeder implements CommandLineRunner {
 
     private final FuncionarioRepository funcionarioRepository;
     private final ProfessorRepository professorRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public DatabaseSeeder(FuncionarioRepository funcionarioRepository, ProfessorRepository professorRepository) {
+    public DatabaseSeeder(
+        FuncionarioRepository funcionarioRepository, 
+        ProfessorRepository professorRepository,
+        UserRepository userRepository,
+        PasswordEncoder passwordEncoder 
+    ) {
         this.funcionarioRepository = funcionarioRepository;
         this.professorRepository = professorRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,47 +38,31 @@ public class DatabaseSeeder implements CommandLineRunner {
 
         if (funcionarioRepository.count() == 0) {
             System.out.println("Populando banco de dados com dados iniciais de funcionários...");
+            
+            Funcionario f1 = new Funcionario("Maria Oliveira", "11122233344", "Bilheteria", TipoVinculo.FIXO);
+            Funcionario f2 = new Funcionario("Pedro Rocha", "55566677788", "Manutenção", TipoVinculo.TEMPORARIO);
+            Funcionario f3 = new Funcionario("João Silva", "44455566677", "TI", TipoVinculo.FIXO);
 
-            Funcionario f1 = new Funcionario(
-                    "Maria Oliveira",
-                    "11122233344",
-                    "Bilheteria",
-                    TipoVinculo.FIXO
-            );
+            Professor p1 = new Professor("Mariana Filho", "99988877766", "Pesquisador", TipoVinculo.TEMPORARIO, "Biologia Molecular", NivelAcademico.MESTRADO);
+            Professor p2 = new Professor("Carlos Mendes", "33322211100", "Pesquisador", TipoVinculo.FIXO, "Astrofísica", NivelAcademico.DOUTORADO);
 
-            Funcionario f2 = new Funcionario(
-                    "Pedro Rocha",
-                    "55566677788",
-                    "Manutenção",
-                    TipoVinculo.TEMPORARIO
-            );
-
-            funcionarioRepository.save(f1);
-            funcionarioRepository.save(f2);
+            funcionarioRepository.saveAll(java.util.List.of(f1, f2, f3));
+            professorRepository.saveAll(java.util.List.of(p1, p2));
         }
 
-        if (professorRepository.count() == 0) {
+        if (!userRepository.findByLogin("teste_jwt").isPresent()) {
+            System.out.println("Criando usuário de teste para JWT...");
 
-            Professor p1 = new Professor(
-                "Mariana Filho",
-                "99988877766",
-                "Pesquisador",
-                TipoVinculo.TEMPORARIO,
-                "Biologia Molecular",
-                NivelAcademico.MESTRADO
-            );
-
-            Professor p2 = new Professor(
-                "Carlos Mendes",
-                "33322211100",
-                "Pesquisador",
-                TipoVinculo.FIXO,
-                "Astrofísica",
-                NivelAcademico.DOUTORADO
-             );
-
-             professorRepository.save(p1);
-             professorRepository.save(p2);
-         }
+            String encodedPassword = passwordEncoder.encode("123"); 
+            
+            User testUser = new User();
+            testUser.setLogin("teste_jwt");
+            testUser.setPassword(encodedPassword);
+            testUser.setRole(UserRole.ADMIN);
+            
+            userRepository.save(testUser); 
+            
+            System.out.println("Usuário de teste JWT ('teste_jwt' / '123') criado com sucesso!");
+        }
     }
 }

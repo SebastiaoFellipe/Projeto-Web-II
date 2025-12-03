c3d5794c8a893a856d9819371e3671f7b2639a17
beatriz-camilo <abeatrizcamilo@gmail.com>
2025-12-02 22:24:28 -0300

    Mudança de localização de classes

--- projetoweb2/src/main/java/com/bti/projetoweb2/security/SecurityConfiguration.java
+++ projetoweb2/src/main/java/com/bti/projetoweb2/security/SecurityConfiguration.java
@@ -1,44 +1,49 @@
 package com.bti.projetoweb2.security;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter; // Importante
 
 @Configuration
-@EnableAutoConfiguration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfiguration(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .maximumSessions(1)
-            .maxSessionsPreventsLogin(false)
-        );
-        
-        http.authorizeHttpRequests(auth -> auth
-            .requestMatchers("/admin/**").hasRole("ADMIN")
-            .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-            .anyRequest().permitAll()
-        )
-        .formLogin(form -> form
-            .loginPage("/login")
-            .defaultSuccessUrl("/user/home", true)
-            .permitAll()
-        )
-        .logout(logout -> logout
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/")
-            .permitAll()
-        );
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                    
+                    
+                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+
+                    
+                    .requestMatchers(HttpMethod.POST, "/animal/**").hasAnyRole("ADMIN", "FUNCIONARIO_COMUM")
+                    .requestMatchers(HttpMethod.GET, "/habitat/**").hasAnyRole("ADMIN", "FUNCIONARIO_COMUM", "PROFESSOR", "CANDIDATO")
+                    .requestMatchers(HttpMethod.DELETE, "/habitat/**").hasRole("ADMIN")
+
+                    
+                    .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
